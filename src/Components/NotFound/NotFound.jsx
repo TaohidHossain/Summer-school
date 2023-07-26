43bac010a2ccea1919791f73482baac8c446f7fa
TaohidHossain <htaohid31@gmail.com>
2023-07-26 12:05:16 +0600

    added Footer and NotFound pages

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,10 +1,10 @@
+import { Link } from "react-router-dom"
 function NotFound(){
-
     return(
-        <div >
-
+        <div>
+            <button className="mx-auto block btn btn-warning my-4 "> <Link to='/'>Go back to home Page</Link> </button>
+            <img className="mx-auto" src="https://thumbs.dreamstime.com/b/error-page-layout-vector-design-website-page-creative-concept-page-you-requested-could-not-be-found-oops-error-page-error-page-131871034.jpg" />
         </div>
     )
 }
-
 export default NotFound;
