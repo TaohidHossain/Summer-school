335010b5b7bd0922e24c140802e29a51e3e79341
TaohidHossain <htaohid31@gmail.com>
2023-07-29 17:50:55 +0600

    added courses and teachers page

--- src/Components/Teachers/Teachers.jsx
+++ src/Components/Teachers/Teachers.jsx
@@ -1,8 +1,48 @@
-function Teachers(){
-
-    return(
-        <div >
-
+import Card from "../Home/Gallery/Card";
+import { useEffect } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+const Teachers = ({/* courses */}) => {
+    const courses = [
+        {
+            _id:1,
+            name: "Math",
+            price: 100,
+            rating: 5,
+            category: "Math"
+        },{
+            _id:2,
+            name: "Violin",
+            price: 100,
+            rating: 5,
+            category: "Math"
+        },{
+            _id:3,
+            name: "Programming",
+            price: 100,
+            rating: 5,
+            category: "Math"
+        },{
+            _id:4,
+            name: "Olympiad",
+            price: 100,
+            rating: 5,
+            category: "Math"
+        }
+    ]
+    useEffect(()=>{
+        AOS.init();
+    },[])
+    console.log(courses);
+    return (
+        <div data-aos="fade-right" className="w-[90%] mx-auto">
+            <div className="gap-2 grid grid-cols-1 md:grid-cols-3">
+            {
+                courses.map(course => {
+                     return <Card  key={course._id} course={course} />
+                })
+            }
+            </div>    
         </div>
     )
 }
